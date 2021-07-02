700ac9d001aee57e1900641b04a1801098ce3861
LaievskyiSasha <zzsanchozz@gmail.com>
2021-07-03 01:51:32 +0300

    add html vue

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+    el: "#input",
+    data: {
+
+    }
+});
+
+
+
+
+
+
+
+ß
+
+
+
+
+
+// let obj = {
+//     1: {
+//         'date': 'date',
+//         'order': 'order type',
+//         'etc': 'etc'
+//     },
+//     2: {}
+// }
