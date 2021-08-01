61e0d5575e960575dbf3d7099b911d8cc325e752
LaievskyiSasha <zzsanchozz@gmail.com>
2021-08-01 23:07:15 +0300

    add vue logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+div class="wrapper">
+        <div class="head">
+            <h2>Please fill the data:</h2>
+        </div>
+        <div class="input" id="input">
+            <input v-model="date" placeholder="date" />
+            <input v-model="order_type" placeholder="Order Type" />
+            <input v-model="market" placeholder="Pair" />
+            <input v-model="size" placeholder="Ammount" />
+            <input v-model="price" placeholder="Price" />
+            <input v-model="fee" placeholder="Date" />
+            <input v-model="fee" placeholder="Fee" />
+            <input v-model="result" placeholder="Result" />
+            <button @click='addOrder'>Submit</button>
+            <!-- <div class="table_data"></div> -->
+        </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const app = new Vue({
+    el: '#input',
+    data: {
+        list: {
+            date: [],
+            order_type: [],
+            market: [],
+            size: [],
+            price: [],
+            fee: [],
+            result: [],
+        },
+        date: null,
+        order_type: null,
+        market: null,
+        size: null,
+        price: null,
+        fee: null,
+        result: null,
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    }
+},
+mounted() {
+    if (localStorage.getItem(ls)) {
+        list = JSON.parse(localStorage.getItem('ls'));
+    }
+},
+
+methods: {
+    addOrder() {
+        //check it something typed
+        if (!this.date && !this.order_type && !this.market && !this.size && !this.price && !this.fee && this.result) return;
+
+        this.list.date.push(this.date);
+        this.list.order_type.push(this.order_type);
+        this.list.market.push(this.market);
+        this.list.size.push(this.size);
+        this.list.price.push(this.price);
+        this.list.fee.push(this.fee);
+        this.list.result.push(this.result);
+
+        // *** clear field ?
+        this.date = '';
+        this.order_type = '';
+        this.market = '';
+        this.size = '';
+        this.price = '';
+        this.fee = '';
+        this.result = '';
+
+        this.saveLocalStorage();
+
+    },
+    saveLocalStorage() {
+        const parsed = JSON.stringify(this.list);
+        localStorage.setItem('list', parsed);
+    }
 }
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/script.js
+++ src/script.js
@@ -1,9 +1,62 @@
 const app = new Vue({
-    el: "#input",
+    el: '#input',
     data: {
+        list: {
+            date: [],
+            order_type: [],
+            market: [],
+            size: [],
+            price: [],
+            fee: [],
+            result: [],
+        },
+        date: null,
+        order_type: null,
+        market: null,
+        size: null,
+        price: null,
+        fee: null,
+        result: null,
 
     }
-});
+},
+mounted() {
+    if (localStorage.getItem(ls)) {
+        list = JSON.parse(localStorage.getItem('ls'));
+    }
+},
+
+methods: {
+    addOrder() {
+        //check it something typed
+        if (!this.date && !this.order_type && !this.market && !this.size && !this.price && !this.fee && this.result) return;
+
+        this.list.date.push(this.date);
+        this.list.order_type.push(this.order_type);
+        this.list.market.push(this.market);
+        this.list.size.push(this.size);
+        this.list.price.push(this.price);
+        this.list.fee.push(this.fee);
+        this.list.result.push(this.result);
+
+        // *** clear field ?
+        this.date = '';
+        this.order_type = '';
+        this.market = '';
+        this.size = '';
+        this.price = '';
+        this.fee = '';
+        this.result = '';
+
+        this.saveLocalStorage();
+
+    },
+    saveLocalStorage() {
+        const parsed = JSON.stringify(this.list);
+        localStorage.setItem('list', parsed);
+    }
+}
+})
 
 
 
@@ -11,17 +64,56 @@ const app = new Vue({
 
 
 
-ß
 
 
 
 
+// //test
+// function localStorage(arr){
+//     let obj = {
+//         1:{
+//         date: date,
+//         order: order_type,
+//         pair:pair,
+//         ammount: 23,
+//         price: 32500,
+//         fee: 0.002,
+//         result: 14,
+//         }, //first array as [date, order type, pair, ammount, price, fee, result]
 
-// let obj = {
-//     1: {
-//         'date': 'date',
-//         'order': 'order type',
-//         'etc': 'etc'
-//     },
-//     2: {}
+//         2:{
+//             1:{
+//                 date: date,
+//                 order: order_type,
+//                 pair:pair,
+//                 ammount: 23,
+//                 price: 32500,
+//                 fee: 0.002,
+//                 result: 14,
+//                 }, //second array as [date, order type, pair, ammount, price, fee, result]
+//         },
+//     }
+
+// }
+
+// function addToLocalStorage(data) {
+//     let localstorage = {};
+//     for (let i = 0; i < arr.length; i++) {
+//         localStorage[i].setItem(date);
+//         localStorage[i].setItem(order);
+//         localStorage[i].setItem(pair);
+//         localStorage[i].setItem(ammount);
+//         localStorage[i].setItem(price);
+//         localStorage[i].setItem(fee);
+//         localStorage[i].setItem(result);
+//     }
+
+//     function executeFromLocalStorage(data) {
+//         for (let i = 0; i < localStorage.length; i++) {
+//             let orderInfo = localStorage.getItem(i);
+//             for (let key in orderIfo) {
+//                 //create new cells and add value to them via executing orderInfo[key];
+//             }
+//         }
+//     }
 // }
