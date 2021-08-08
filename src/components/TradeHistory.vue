88c3ef3a45c767181763230fa4f4804f3ad7fb36
LaievskyiSasha <zzsanchozz@gmail.com>
2021-08-09 01:28:44 +0300

    trying add vue logic

---
+++ src/components/TradeHistory.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrapper">
+        <div class="head">
+            <h2>Please fill the data:</h2>
+        </div>
+        <div class="input" id="input">
+            <input v-model="date" placeholder="date" />
+            <input v-model="order_type" placeholder="Order Type" />
+            <input v-model="market" placeholder="Pair" />
+            <input v-model="size" placeholder="Ammount" />
+            <input v-model="price" placeholder="Price" />
+            <input v-model="fee" placeholder="Date" />
+            <input v-model="fee" placeholder="Fee" />
+            <input v-model="result" placeholder="Result" />
+            <button @click='addOrder'>Submit</button>
+            <!-- <div class="table_data"></div> -->
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TradeHistory',
+  props: {
+    msg: String
+  }
+}
+</script>
